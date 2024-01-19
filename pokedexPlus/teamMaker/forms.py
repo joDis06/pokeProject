2475fe1e6f3f21123531be2077967af98b7dc4d1
joDis06 <jdisomma40@stuy.edu>
2024-01-19 15:18:04 -0500

    Pokedex ATK broken;TeamDetail has MC but no limit, shows name & pic now

--- pokedexPlus/teamMaker/forms.py
+++ pokedexPlus/teamMaker/forms.py
@@ -3,7 +3,14 @@ from django import forms
 from .models import Team
 from pokedex.models import Pokemon
 
+#validators
+
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
         fields = ['teamPKMN']
+        
+    teamPKMN = forms.ModelMultipleChoiceField(
+        queryset=Pokemon.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )

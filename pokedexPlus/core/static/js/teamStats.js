e1f5467f9c70c276a22e3740d69be0e94b6c208e
Phoebe Young <pyoung40@stuy.edu>
2024-01-21 22:54:57 -0500

    "So: Problem I realized! How exactly do I get all the API stuff into the JS...?"

--- pokedexPlus/core/static/js/teamStats.js
+++ pokedexPlus/core/static/js/teamStats.js
@@ -1,19 +1,23 @@
-let teamButton1 = ""
-let teamButton2 = ""
+// let teamButton1 = ""
+// let teamButton2 = ""
 
-document.querySelector(".teamButton1").addEventListener('click', function() {
-    // teamButton1 = document.querySelector().value;
+// document.querySelector(".teamButton").addEventListener('click', function() {
+//     teamButton1 = document.querySelector(".teamButton").value;
+//     if(teamButton1 != "") {
+//         console.log("messed up, restart")
+//     } else{
+//         console.log(teamButton1);
+//     }
+//     // make the value = to object id
+// })
 
-    // make the value = to object id
-})
+// document.querySelector(".teamButton2").addEventListener('click', function() {
+//     // teamButton1 = document.querySelector().value;
 
-document.querySelector(".teamButton2").addEventListener('click', function() {
-    // teamButton1 = document.querySelector().value;
+//     // make the value = to object id
+// })
 
-    // make the value = to object id
-})
-
-document.querySelector(".compareTeams").addEventListener('click', function() {
-    // display the 2 teams vertically side-by-side
-})
+// document.querySelector(".compareTeams").addEventListener('click', function() {
+//     // display the 2 teams vertically side-by-side
+// })
 

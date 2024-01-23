34dfffdec0fbbd16a1ff9eebdd5b9aa0d41fb392
Phoebe Young <pyoung40@stuy.edu>
2024-01-22 19:34:57 -0500

    added javascript

--- pokedexPlus/teamStats/views.py
+++ pokedexPlus/teamStats/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.core import serializers
+
 
 from pokedex.models import Pokemon, Types
 from teamMaker.models import Team
@@ -15,6 +17,10 @@ import requests, json
 def teamStatsView(request):
     pokemon = Pokemon.objects.all()
     teams = Team.objects.all()
+    teamsJSON = serializers.serialize("json", Team.objects.all())
+    pkmnJSON = serializers.serialize("json", Pokemon.objects.all())
 
     return render(request, 'teamStats/teamStats.html', context={"pokemon": pokemon,
-                                                                "teams" : teams})
+                                                                "teams" : teams,
+                                                                "teamsJSON" : teamsJSON,
+                                                                "pkmnJSON": pkmnJSON})
